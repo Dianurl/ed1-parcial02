7728ad1a9bcb91ace1fcc4883061cf89aee92753
Ejercicio1 <diandiesurl@gmail.com>
2025-05-23 08:32:27 -0600

    Parcial2

--- src/main/java/ed/lab/E01MeetingRooms.java
+++ src/main/java/ed/lab/E01MeetingRooms.java
@@ -4,33 +4,42 @@ import java.util.List;
 
 
 public class E01MeetingRooms {
+    int start;
+    int end;
+
+    public void MeetingInterval(int start, int end) {
+        this.start = start;
+        this.end = end;
+    }
 
     public int minMeetingRooms(List<MeetingInterval> meetingIntervals) {
-        if (meetingIntervals == null || meetingIntervals.List<MeetingInterval> == 0)
-            return 0;
 
-        int n = meetingIntervals.length;
+
+        if (meetingIntervals == null || meetingIntervals.isEmpty()) return 0;
+
+        int n = meetingIntervals.size();
         int[] startTimes = new int[n];
         int[] endTimes = new int[n];
 
         for (int i = 0; i < n; i++) {
-            startTimes[i] = meetingIntervals.get(i)[0];
-            endTimes[i] = meetingIntervals.get(i)[1];
+            startTimes[i] = meetingIntervals.get(i).start;
+            endTimes[i] = meetingIntervals.get(i).end;
         }
 
         Arrays.sort(startTimes);
         Arrays.sort(endTimes);
 
         int start = 0, end = 0;
-        int rooms = 0, maxRooms = 0;
+        int usedRooms = 0;
+        int maxRooms = 0;
 
         while (start < n) {
             if (startTimes[start] < endTimes[end]) {
-                rooms++;
-                maxRooms = Math.max(maxRooms, rooms);
+                usedRooms++;
+                maxRooms = Math.max(maxRooms, usedRooms);
                 start++;
             } else {
-                rooms--;
+                usedRooms--;
                 end++;
             }
         }
