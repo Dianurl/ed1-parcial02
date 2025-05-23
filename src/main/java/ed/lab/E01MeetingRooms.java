eb14ba5f44eb1cb37e555a7d57f8be6e21f1628f
Ejercicio1 <diandiesurl@gmail.com>
2025-05-23 08:56:47 -0600

    Parcial2 - Diego Diaz - 1214024

--- src/main/java/ed/lab/E01MeetingRooms.java
+++ src/main/java/ed/lab/E01MeetingRooms.java
@@ -1,50 +1,24 @@
 package ed.lab;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 
 public class E01MeetingRooms {
-    int start;
-    int end;
-
-    public void MeetingInterval(int start, int end) {
-        this.start = start;
-        this.end = end;
-    }
-
     public int minMeetingRooms(List<MeetingInterval> meetingIntervals) {
-
-
-        if (meetingIntervals == null || meetingIntervals.isEmpty()) return 0;
-
-        int n = meetingIntervals.size();
-        int[] startTimes = new int[n];
-        int[] endTimes = new int[n];
-
-        for (int i = 0; i < n; i++) {
-            startTimes[i] = meetingIntervals.get(i).start;
-            endTimes[i] = meetingIntervals.get(i).end;
+        if (meetingIntervals == null || meetingIntervals.isEmpty()) {
+            return 0;
         }
+        meetingIntervals.sort(Comparator.comparingInt(MeetingInterval::startTime));
 
-        Arrays.sort(startTimes);
-        Arrays.sort(endTimes);
+        PriorityQueue<Integer> TheEnd = new PriorityQueue<>();
 
-        int start = 0, end = 0;
-        int usedRooms = 0;
-        int maxRooms = 0;
-
-        while (start < n) {
-            if (startTimes[start] < endTimes[end]) {
-                usedRooms++;
-                maxRooms = Math.max(maxRooms, usedRooms);
-                start++;
-            } else {
-                usedRooms--;
-                end++;
+        for (MeetingInterval interval : meetingIntervals) {
+            if (!TheEnd.isEmpty() && TheEnd.peek() <= interval.startTime()) {
+                TheEnd.poll();
             }
-        }
 
-        return maxRooms;
+            TheEnd.add(interval.endTime());
+        }
+        return TheEnd.size();
     }
 
 }
