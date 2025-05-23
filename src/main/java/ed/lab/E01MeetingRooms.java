26d02256822bebd8ad604af1e91d964e127b5489
Ejercicio1 <diandiesurl@gmail.com>
2025-05-23 08:29:16 -0600

    Parcial2

--- src/main/java/ed/lab/E01MeetingRooms.java
+++ src/main/java/ed/lab/E01MeetingRooms.java
@@ -1,11 +1,41 @@
 package ed.lab;
-
+import java.util.Arrays;
 import java.util.List;
+
 
 public class E01MeetingRooms {
 
     public int minMeetingRooms(List<MeetingInterval> meetingIntervals) {
-        return -1;
+        if (meetingIntervals == null || meetingIntervals.List<MeetingInterval> == 0)
+            return 0;
+
+        int n = meetingIntervals.length;
+        int[] startTimes = new int[n];
+        int[] endTimes = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            startTimes[i] = meetingIntervals.get(i)[0];
+            endTimes[i] = meetingIntervals.get(i)[1];
+        }
+
+        Arrays.sort(startTimes);
+        Arrays.sort(endTimes);
+
+        int start = 0, end = 0;
+        int rooms = 0, maxRooms = 0;
+
+        while (start < n) {
+            if (startTimes[start] < endTimes[end]) {
+                rooms++;
+                maxRooms = Math.max(maxRooms, rooms);
+                start++;
+            } else {
+                rooms--;
+                end++;
+            }
+        }
+
+        return maxRooms;
     }
 
 }
--- src/main/java/ed/lab/E02Logger.java
+++ src/main/java/ed/lab/E02Logger.java
@@ -1,11 +1,32 @@
 package ed.lab;
 
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class E02Logger {
+    Map<String, Integer> hash;
+    int time;
+    int count;
 
     public E02Logger() {
+        this.hash = new HashMap<>();
+        this.time = 0;
+        this.count = 10;
     }
 
     public boolean shouldPrintMessage(int timestamp, String message) {
+        if(hash.get(message) == null) {
+            hash.put(message, count + timestamp);
+            return true;
+        }
+
+        if(hash.containsKey(message)) {
+            if(timestamp >= hash.get(message)) {
+                hash.put(message, count + timestamp);
+                return true;
+            }
+        }
         return false;
     }
 
